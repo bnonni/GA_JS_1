db010cf021101e9d439f31af50e392afddadd01c
bnonni <bryanwnonni@gmail.com>
2016-11-10 09:09:20 -0500

    new

--- 99bottles/scripts/Brew.js
+++ 99bottles/scripts/Brew.js
@@ -1,7 +1,13 @@
-/**
- * Created by Bnonni on 11/8/16.
- */
-
 $(document).ready(function(){
-    $().append("99 bottles of beer")
-}
+    for (var i = 99; i >= 2; i--){
+        var lyrics1 = " bottles of beer on the wall, ";
+        var lyrics2 = " bottles of beer.";
+        var lyrics3 = "Take one down and pass it around, ";
+        var lyrics4 = " bottles of beer on the wall.";
+        $('body').append('<p><li>' + i + lyrics1 + i + lyrics2 + '</li></p>' + '<p><li>' + lyrics3 + (i - 1) + lyrics4 + '</li></p>');
+    }
+    while (i < 2){
+        $('body').append('<p><li>' + '1 bottle of beer on the wall, 1 bottle of beer.' + '</li></p>' + '<p><li>'+ 'Take one down and pass it around, no more bottles of beer on the wall.' + '</li></p>' + '<p><li>' + 'No more bottles of beer on the wall, no more bottles of beer.' + '</li></p>' + '<p><li>' + 'Go to the store and buy some more, 99 bottles of beer on the wall.' + '</li></p>');
+    break;}
+});
+
